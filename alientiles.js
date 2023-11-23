6b5da2d55042b31d2aaa0beb671a7dd581f2ad11
Robert Setter <r.setter@comcast.net>
2023-11-23 04:11:31 -0600

    trade-off
    Removing the initial color selection as it is disrupting the rest of the logic.
    Adding undo implementation.

--- alientiles.js
+++ alientiles.js
@@ -1,30 +1,21 @@
 var tiles;
 var dimension;
 const COLORS = 4;
-//const START_COLOR = 0;
+var actionStack = new Array();
+const START_COLOR = 0;
 
 var tile_color = {
-    '0': 'red',
-    '1': 'green',
-    '2': 'blue',
-    '3': 'purple'
-    }
+	'0': 'red',
+	'1':'green',
+	'2':'blue',
+	'3':'purple'
+}
+
 
 function createTable() {
   document.getElementById("table").innerHTML = "";
   dimension = document.getElementById("dimension").value;
-  if (document.getElementById("colorComboBox").selectedIndex != 1) 
-  {
-	  tiles = new Array(dimension*dimension).fill(document.getElementById("colorComboBox").value);
-  }
-  else
-  {
-	  tiles = new Array(dimension*dimension);
-	  for (var t = 0; t < tiles.length; t++)
-	  {
-		  tiles[t] = Math.floor(Math.random() * 4);
-	  }
-  }
+  tiles = new Array(dimension*dimension).fill(START_COLOR);
   document.documentElement.style.setProperty("--dimension", dimension);
   document.getElementById("table").style["grid-template-columns"] =  'repeat(var(--dimension), auto)';
   for (var i = 0; i < dimension*dimension; i++) {
@@ -33,21 +24,30 @@ function createTable() {
     button.id = "button" + i;
     button.style["background-color"] = tile_color[tiles[i]];
     document.getElementById("table").appendChild(button);
-    button.onclick = function() {updateColors(this.id)};
+    button.onclick = function() {updateColors(this.id, 1)};
   }
 }
 
-  function updateColors(buttonID) {
+function undoAction() {
+	updateColors(actionStack.pop(), -1);
+	document.getElementById("actionCnt").value = actionStack.length;
+}
+
+  function updateColors(buttonID, dir) {
+	  if (dir == 1) {
+		  actionStack.push(buttonID);
+		  document.getElementById("actionCnt").value = actionStack.length;
+	  }
     var id = parseInt(buttonID.substring(6));
     var row = parseInt(Math.floor(id/dimension));
     var col = parseInt(id % dimension);
     for (var i = dimension*row; i < (dimension*row)+ parseInt(dimension); i++) {
-      tiles[i] = (tiles[i] + 1) % COLORS;
+      tiles[i] = (tiles[i] + dir) % COLORS;
       document.getElementById("button"+i).style["background-color"] = tile_color[tiles[i]];
     }
     for (var i = col; i < dimension*dimension; i=i+parseInt(dimension)) {
       if (i != id) {
-        tiles[i] = (tiles[i] + 1) % COLORS;
+        tiles[i] = (tiles[i] + dir) % COLORS;
         document.getElementById("button"+i).style["background-color"] = tile_color[tiles[i]];
       }
     }
