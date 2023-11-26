36b426785aa3f6046ae19892a5563f596e099636
Robert Setter <r.setter@comcast.net>
2023-11-26 02:04:05 -0600

    JS housekeeping

--- alientiles.js
+++ alientiles.js
@@ -1,9 +1,7 @@
-let tiles = new Array();
-var dimension;
 const COLORS = 4;
+var dimension;
+var tiles = new Array();
 var actionStack = new Array();
-const START_COLOR = 0;
-
 var tile_color = [
 	'red',
 	'green',
@@ -11,7 +9,8 @@ var tile_color = [
 	'purple'
 ]
 
-function createTable() {
+function createTable()
+{
   var colorComboBoxSelection = document.getElementById("colorComboBox").value; 
   document.getElementById("actionCnt").value = actionStack.length = 0;
   document.getElementById("table").innerHTML = "";
@@ -31,14 +30,11 @@ function createTable() {
 		  var stageTile = new Object();
 		  stageTile.tile = document.getElementById("table").appendChild(button);
 		  stageTile.colorIndex = colorSelection;
-		  stageTile.id = parseInt(stageTile.tile.id.substring(6));
 		  stageTile.row = i;
 		  stageTile.col = j;
 		  tiles[i][j] = stageTile;
 		  tiles[i][j].tile.style["background-color"] = tile_color[tiles[i][j].colorIndex];
 	  }
-    
-    //button.onclick = function() {updateColors(this.id, 1)};
   }
 }
 
@@ -84,32 +80,32 @@ function updateColors(targetTile, undo)
 	}
 }
 
-  window.onload = function(){
-    var dim = document.getElementById("dimension");
+window.onload = function(){
+	var dim = document.getElementById("dimension");
     dim.addEventListener("keyup", function(event) {
-      event.preventDefault();
-      // Number 13 is the "Enter" key on the keyboard
-      if (event.keyCode === 13) {
-        // Trigger the button element with a click
-        document.getElementById("submit").click();
-      }
-      });
-    };
-	
-	document.getElementById("table").addEventListener("click", function(evt) {
-		var elem = evt.target;
-		//alert(`Clicked ID: ${elem.id}`)
-		var clickedBtn;
-		if (elem.tagName == 'BUTTON')
+		event.preventDefault();
+		// Number 13 is the "Enter" key on the keyboard
+		if (event.keyCode === 13)
 		{
-			for (i = 0; i < dimension; i++)
-			{
-				clickedBtn = tiles[i].findLast(x => x.tile.id == elem.id)
-				if (typeof clickedBtn !== 'undefined')
-					break;
-			}
-			actionStack.push(clickedBtn);
-			document.getElementById("actionCnt").value = actionStack.length;
-			updateColors(clickedBtn, false);
+			// Trigger the button element with a click
+			document.getElementById("submit").click();
 		}
 	});
+};
+
+document.getElementById("table").addEventListener("click", function(evt) {
+	var elem = evt.target;
+	var clickedBtn;
+	if (elem.tagName == 'BUTTON')
+	{
+		for (i = 0; i < dimension; i++)
+		{
+			clickedBtn = tiles[i].findLast(x => x.tile.id == elem.id)
+			if (typeof clickedBtn !== 'undefined')
+				break;
+		}
+		actionStack.push(clickedBtn);
+		document.getElementById("actionCnt").value = actionStack.length;
+		updateColors(clickedBtn, false);
+	}
+});
