16c6d6e71e37816d90b6b6f44efb0484ee3bd729
Robert Setter <r.setter@comcast.net>
2023-11-26 01:33:37 -0600

    Java-based refactor
    Rewrote and restructured this code base to better align with the Java code used in my old class project.
    Gets both the undo functionality and the color selection mostly functional.

--- alientiles.js
+++ alientiles.js
@@ -1,59 +1,88 @@
-var tiles;
+let tiles = new Array();
 var dimension;
 const COLORS = 4;
 var actionStack = new Array();
 const START_COLOR = 0;
 
-var tile_color = {
-	'0': 'red',
-	'1':'green',
-	'2':'blue',
-	'3':'purple'
-}
-
+var tile_color = [
+	'red',
+	'green',
+	'blue',
+	'purple'
+]
 
 function createTable() {
+  var colorComboBoxSelection = document.getElementById("colorComboBox").value; 
   document.getElementById("actionCnt").value = actionStack.length = 0;
   document.getElementById("table").innerHTML = "";
   dimension = document.getElementById("dimension").value;
-  tiles = new Array(dimension*dimension).fill(START_COLOR);
   document.documentElement.style.setProperty("--dimension", dimension);
   document.getElementById("table").style["grid-template-columns"] =  'repeat(var(--dimension), auto)';
-  for (var i = 0; i < dimension*dimension; i++) {
-    var button = document.createElement("button");
-    button.className = "grid-item";
-    button.id = "button" + i;
-    button.style["background-color"] = tile_color[tiles[i]];
-    document.getElementById("table").appendChild(button);
-    button.onclick = function() {updateColors(this.id, 1)};
+  var k = 0;
+  for (var i = 0; i < dimension; i++) {
+	  tiles[i] = new Array(dimension);
+	  for (j = 0; j < dimension; j++)
+	  {
+		  var button = document.createElement("button");
+		  button.className = "grid-item";
+		  button.id = "button" + k;
+		  k++;
+		  var colorSelection = colorComboBoxSelection < 4 ? colorComboBoxSelection : Math.floor(Math.random() * COLORS);
+		  var stageTile = new Object();
+		  stageTile.tile = document.getElementById("table").appendChild(button);
+		  stageTile.colorIndex = colorSelection;
+		  stageTile.id = parseInt(stageTile.tile.id.substring(6));
+		  stageTile.row = i;
+		  stageTile.col = j;
+		  tiles[i][j] = stageTile;
+		  tiles[i][j].tile.style["background-color"] = tile_color[tiles[i][j].colorIndex];
+	  }
+    
+    //button.onclick = function() {updateColors(this.id, 1)};
   }
 }
 
-function undoAction() {
+function toggle(backward, tileObj)
+{
+	if (tileObj.colorIndex == tile_color.length - 1)
+		tileObj.colorIndex = 0;
+	else if (backward && tileObj.colorIndex > 0)
+		tileObj.colorIndex--;
+	else if (backward && tileObj.colorIndex == 0) 
+		tileObj.colorIndex = tile_color.length - 1;
+	else
+		tileObj.colorIndex++;
+	tileObj.tile.style["background-color"] = tile_color[tileObj.colorIndex];
+}
+
+function undoAction()
+{
 	if (actionStack.length > 0)
-		updateColors(actionStack.pop(), -1);
+	{
+		updateColors(actionStack.pop(), true);
+	}
 	document.getElementById("actionCnt").value = actionStack.length;
 }
 
-  function updateColors(buttonID, dir) {
-	  if (dir == 1) {
-		  actionStack.push(buttonID);
-		  document.getElementById("actionCnt").value = actionStack.length;
-	  }
-    var id = parseInt(buttonID.substring(6));
-    var row = parseInt(Math.floor(id/dimension));
-    var col = parseInt(id % dimension);
-    for (var i = dimension*row; i < (dimension*row)+ parseInt(dimension); i++) {
-      tiles[i] = (tiles[i] + dir) % COLORS;
-      document.getElementById("button"+i).style["background-color"] = tile_color[tiles[i]];
-    }
-    for (var i = col; i < dimension*dimension; i=i+parseInt(dimension)) {
-      if (i != id) {
-        tiles[i] = (tiles[i] + dir) % COLORS;
-        document.getElementById("button"+i).style["background-color"] = tile_color[tiles[i]];
-      }
-    }
-  }
+function updateColors(targetTile, undo)
+{
+	for (i=0; i<=targetTile.row; i++)
+	 {
+		 toggle(undo, tiles[i][targetTile.col]);
+	}
+	for (i=targetTile.row+1; i<dimension; i++)
+	{
+		toggle(undo, tiles[i][targetTile.col]);
+	}
+	for (i=0; i<targetTile.col; i++)
+	{
+		toggle(undo, tiles[targetTile.row][i]);
+	}
+	for (i=targetTile.col+1; i<dimension; i++)
+	{
+		toggle(undo, tiles[targetTile.row][i]);
+	}
+}
 
   window.onload = function(){
     var dim = document.getElementById("dimension");
@@ -66,3 +95,21 @@ function undoAction() {
       }
       });
     };
+	
+	document.getElementById("table").addEventListener("click", function(evt) {
+		var elem = evt.target;
+		//alert(`Clicked ID: ${elem.id}`)
+		var clickedBtn;
+		if (elem.tagName == 'BUTTON')
+		{
+			for (i = 0; i < dimension; i++)
+			{
+				clickedBtn = tiles[i].findLast(x => x.tile.id == elem.id)
+				if (typeof clickedBtn !== 'undefined')
+					break;
+			}
+			actionStack.push(clickedBtn);
+			document.getElementById("actionCnt").value = actionStack.length;
+			updateColors(clickedBtn, false);
+		}
+	});
